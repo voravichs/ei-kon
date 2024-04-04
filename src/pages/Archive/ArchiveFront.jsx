f672274b28b53391fcafee4d7d37b2de896c75dc
VoravichS <vsilapac@gmail.com>
2024-04-04 14:56:30 -0400

    keywords archives

--- src/pages/Archive/ArchiveFront.jsx
+++ src/pages/Archive/ArchiveFront.jsx
@@ -8,7 +8,7 @@ import { Link } from "react-router-dom";
 export default function ArchiveFront() {
     return (
         <div className="h-dvh px-32 bg-card-accent py-8 flex flex-col gap-8">
-            <h1 className="flex-center text-7xl font-bona-nova tracking-widest">
+            <h1 className="flex-center text-7xl font-bona-nova font-bold tracking-widest">
                 ARCHIVE
             </h1>
             <div className="flex-center gap-4">
@@ -19,11 +19,11 @@ export default function ArchiveFront() {
             <div className="w-full flex justify-center gap-8">
                 <Link to={"keywords"} className="bg-secondary text-white font-bold rounded-lg p-8 w-1/2 2xl:w-1/3 drop-shadow-lg flex items-center gap-4">
                     <FaBook className="text-4xl" />
-                    <p className="font-bona-nova text-4xl">{"Keywords"}</p>
+                    <p className="font-bona-nova text-4xl">KEYWORDS</p>
                 </Link>
                 <Link to={"classes"} className="bg-secondary text-white font-bold rounded-lg p-8 w-1/2 2xl:w-1/3 drop-shadow-lg flex items-center gap-4">
                     <GiSwordsEmblem className="text-4xl" />
-                    <p className="font-bona-nova text-4xl">{"Classes"}</p>
+                    <p className="font-bona-nova text-4xl">CLASSES</p>
                 </Link>
             </div>
         </div>
--- src/pages/Archive/ArchiveKeywords.jsx
+++ src/pages/Archive/ArchiveKeywords.jsx
@@ -1,38 +1,112 @@
 import keywords from "../../data/keywords"
 import { FaSearch } from "react-icons/fa";
+import { useState } from "react";
+
 /** 
  * Keywords Page
  */
 export default function ArchiveKeywords() {
-    const { statusConditions } = keywords;
+    const { statusConditions, rules, combatGlossary } = keywords;
     const statusConditionsList = Object.keys(statusConditions).map(key => statusConditions[key]);
+    const rulesList = Object.keys(rules).map(key => rules[key]);
+    const combatGlossaryList = Object.keys(combatGlossary).map(key => combatGlossary[key]);
 
-    statusConditionsList.forEach(element => {
-        console.log(element.name)
-    });
+    const [conditionActive, setConditionActive] = useState(true);
+    const [rulesActive, setRulesActive] = useState(false);
+    const [combatActive, setCombatActive] = useState(false);
 
     return (
         <div className="h-dvh px-32 bg-card-accent py-8 flex flex-col gap-8">
-            <div className="text-4xl font-bona-nova font-bold"> Conditions </div>
+            <h1 className="flex-center text-5xl font-bona-nova font-bold tracking-widest">
+                KEYWORDS
+            </h1>
+            <div className="flex gap-8">
+                {conditionActive
+                    ? <div className="text-4xl font-bona-nova font-bold drop-shadow-glow-green pointer-events-none"> Conditions </div>
+                    : <div onClick={() => {
+                        setConditionActive(true);
+                        setRulesActive(false);
+                        setCombatActive(false);
+                    }} className="text-4xl font-bona-nova cursor-pointer"> Conditions </div>
+                }
+                {rulesActive
+                    ? <div className="text-4xl font-bona-nova font-bold drop-shadow-glow-green pointer-events-none"> Rules </div>
+                    : <div onClick={() => {
+                        setConditionActive(false);
+                        setRulesActive(true);
+                        setCombatActive(false);
+                    }} className="text-4xl font-bona-nova cursor-pointer"> Rules </div>
+                }
+                {combatActive
+                    ? <div className="text-4xl font-bona-nova font-bold drop-shadow-glow-green pointer-events-none"> Combat Glossary </div>
+                    : <div onClick={() => {
+                        setConditionActive(false);
+                        setRulesActive(false);
+                        setCombatActive(true);
+                    }} className="text-4xl font-bona-nova cursor-pointer"> Combat Glossary </div>
+                }
+            </div>
+
+
             <div className="flex items-center gap-4">
                 <FaSearch className="text-lg opacity-50" />
                 <input className="border rounded-full text-xl py-2 px-8 drop-shadow-lg font-noto-sans"
                     placeholder={"search keywords"} />
             </div>
-            <div className="flex gap-8 flex-wrap font-bona-nova text-2xl">
-                {statusConditionsList.map(item => {
-                    return (
-                        <div
-                            className="bg-secondary text-white font-bold rounded-lg p-4"
-                            key={item.name}
-                        >
-                            {item.name}
-                        </div>
-                    );
-                })}
-            </div>
-            
-
+            {conditionActive &&
+                <div className="flex gap-8 flex-wrap font-bona-nova">
+                    {statusConditionsList.map(item => {
+                        return (
+                            <div
+                                className="w-52 h-16 bg-secondary text-white rounded-lg p-4 group relative cursor-pointer"
+                                key={item.name}
+                            >
+                                <p className="font-bold text-2xl text-center">{item.name}</p>
+                                <span className="absolute z-10 w-60 bottom-[4.5rem] -left-4 scale-0 rounded bg-gray-800 p-2 text-sm font-noto-sans text-white group-hover:scale-100">
+                                    <p className="font-bold">{item.name}</p>
+                                    <p>{item.desc}</p>
+                                </span>
+                            </div>
+                        );
+                    })}
+                </div>
+            }
+            {rulesActive &&
+                <div className="flex gap-8 flex-wrap font-bona-nova">
+                    {rulesList.map(item => {
+                        return (
+                            <div
+                                className="w-52 h-16 bg-secondary text-white rounded-lg p-4 group relative cursor-pointer"
+                                key={item.name}
+                            >
+                                <p className="font-bold text-2xl text-center">{item.name}</p>
+                                <span className="absolute z-10 w-60 bottom-[4.5rem] -left-4 scale-0 rounded bg-gray-800 p-2 text-sm font-noto-sans text-white group-hover:scale-100">
+                                    <p className="font-bold">{item.name}</p>
+                                    <p>{item.desc}</p>
+                                </span>
+                            </div>
+                        );
+                    })}
+                </div>
+            }
+            {combatActive &&
+                <div className="flex gap-8 flex-wrap font-bona-nova">
+                    {combatGlossaryList.map(item => {
+                        return (
+                            <div
+                                className="w-52 h-16 bg-secondary text-white rounded-lg p-4 group relative cursor-pointer"
+                                key={item.name}
+                            >
+                                <p className="font-bold text-xl text-center">{item.name}</p>
+                                <span className="absolute z-10 w-60 bottom-[4.5rem] -left-4 scale-0 rounded bg-gray-800 p-2 text-sm font-noto-sans text-white group-hover:scale-100">
+                                    <p className="font-bold">{item.name}</p>
+                                    <p>{item.desc}</p>
+                                </span>
+                            </div>
+                        );
+                    })}
+                </div>
+            }
         </div>
     )
 }
