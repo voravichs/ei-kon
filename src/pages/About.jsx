faec97abf1aeaaccccfa8d7c19cff22a684ada49
VoravichS <vsilapac@gmail.com>
2024-04-19 05:25:25 -0400

    embed survey

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,8 +3,9 @@
  */
 export default function About() {
     return (
-        <>
-
-        </>
+        <div className="h-dvh pt-32">
+            <iframe className="w-full h-full" 
+                src="https://docs.google.com/forms/d/e/1FAIpQLSeCueXY1m1Br-eNSWLy3q5UyCd3CbmbqRLOxvPnIWMdAc5msg/viewform?embedded=true">Loading…</iframe>
+        </div>
     )
 }
